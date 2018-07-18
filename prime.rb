3c3a6ad4f85a31ca052e4bb19b3c729784d1d950
Yana Ustsinovich <yustinovich@icloud.com>
2018-07-18 16:36:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+ num = num.abs
+  if num == 0 || num == 1
+   return false
+  elsif num == 2 || num == 3
+   return true
+  end
+ prevnum = (2..num-1).to_a
+ prevnum.any? { |del| num % del != 0 }
+end
